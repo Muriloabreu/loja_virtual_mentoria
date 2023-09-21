16001d5ba868682faf17831fb61091ca8edbb1f7
muriloabreu <murilo.lilo@hotmail.com>
2023-09-20 21:16:02 -0300

    implementação na classe MarcaProduto. ToString

--- src/main/java/com/api/lojavirtual/models/MarcaProduto.java
+++ src/main/java/com/api/lojavirtual/models/MarcaProduto.java
@@ -1,6 +1,7 @@
 package com.api.lojavirtual.models;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -48,6 +49,28 @@ public class MarcaProduto implements Serializable{
 
 	public void setNomeDesc(String nomeDesc) {
 		this.nomeDesc = nomeDesc;
+	}
+
+	@Override
+	public String toString() {
+		return "MarcaProduto [id=" + id + ", nomeDesc=" + nomeDesc + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MarcaProduto other = (MarcaProduto) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
