3eab32553b81e2411a6e92eda0192c3fbcb20839
muriloabreu <murilo.lilo@hotmail.com>
2023-09-21 22:16:33 -0300

    Criação da classe CategoriaProduto

---
+++ src/main/java/com/api/lojavirtual/models/Categoria_Produto.java
@@ -0,0 +1,84 @@
+package com.api.lojavirtual.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class Categoria_Produto implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	private Long id;
+	@Column(name = "nome_desc", nullable = false)
+	private String nome_desc;
+	
+	/* Constructor */
+
+	public Categoria_Produto() {
+	}
+
+	public Categoria_Produto(Long id, String nome_desc) {
+		this.id = id;
+		this.nome_desc = nome_desc;
+	}
+	
+	/* Accessor Methods */
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome_desc() {
+		return nome_desc;
+	}
+
+	public void setNome_desc(String nome_desc) {
+		this.nome_desc = nome_desc;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria_Produto [id=" + id + ", nome_desc=" + nome_desc + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria_Produto other = (Categoria_Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
