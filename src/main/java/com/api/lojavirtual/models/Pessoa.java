94337cc7f6efda3eb591b8357da0c400db69bae7
muriloabreu <murilo.lilo@hotmail.com>
2023-09-21 22:43:27 -0300

    Implementação na classe Pessoa abstract

--- src/main/java/com/api/lojavirtual/models/Pessoa.java
+++ src/main/java/com/api/lojavirtual/models/Pessoa.java
@@ -8,13 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "pessoa")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa", allocationSize = 1, initialValue = 1	)
-public class Pessoa implements Serializable{
+public abstract class Pessoa implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	@Id
