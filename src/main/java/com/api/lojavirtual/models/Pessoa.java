43211bb9900fc5839f382d1e46aa2b688160d53b
muriloabreu <murilo.lilo@hotmail.com>
2023-09-22 22:04:53 -0300

    Implementação e add de variaveis na classe Pessoa

--- src/main/java/com/api/lojavirtual/models/Pessoa.java
+++ src/main/java/com/api/lojavirtual/models/Pessoa.java
@@ -1,20 +1,26 @@
 package com.api.lojavirtual.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+
+
+
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) /* SERVE PARA NÃO CRIAR UMA TABELA NO BANCO*/
 @SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa", allocationSize = 1, initialValue = 1	)
 public abstract class Pessoa implements Serializable{
 	
@@ -26,6 +32,8 @@ public abstract class Pessoa implements Serializable{
 	private String nome;
 	private String email;
 	private String telefone;
+	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Endereco> enderecos = new ArrayList<>();
 	
 	/* Constructor */
 	
@@ -72,10 +80,17 @@ public abstract class Pessoa implements Serializable{
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
 
 	@Override
 	public String toString() {
-		return "Pessoa [id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + "]";
+		return "Pessoa [id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", enderecos="
+				+ enderecos + "]";
 	}
 
 	@Override
