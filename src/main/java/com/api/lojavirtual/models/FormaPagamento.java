030845a0c2869399cd566d2219733a4100aa05bd
muriloabreu <murilo.lilo@hotmail.com>
2023-09-27 22:47:23 -0300

    Criação de Classe FormaPagamento

---
+++ src/main/java/com/api/lojavirtual/models/FormaPagamento.java
@@ -0,0 +1,83 @@
+package com.api.lojavirtual.models;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "forma_pagamento")
+@SequenceGenerator(name = "seq_forma_pagamento", sequenceName = "seq_forma_pagamento", allocationSize = 1, initialValue = 1	)
+public class FormaPagamento implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_forma_pagamento")
+	private Long id;
+	@Column(nullable = false)
+	private String descricao; /* Acesso ex.: ROLE_ADMIN ou ROLE_SECRETARIO*/
+	
+
+	/* Constructor */
+	
+	public FormaPagamento() {
+		
+	}
+	public FormaPagamento(Long id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+	
+	/* Accessor Methods */
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Acesso [id=" + id + ", descricao=" + descricao + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FormaPagamento other = (FormaPagamento) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+	
+
+}
