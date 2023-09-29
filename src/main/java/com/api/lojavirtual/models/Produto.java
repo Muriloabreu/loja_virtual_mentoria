cc4d12aa1450dff645bf9389c808e679abeba6c6
muriloabreu <murilo.lilo@hotmail.com>
2023-09-28 22:41:10 -0300

    Add de mais atributos e metodos acessores

--- src/main/java/com/api/lojavirtual/models/Produto.java
+++ src/main/java/com/api/lojavirtual/models/Produto.java
@@ -2,6 +2,7 @@ package com.api.lojavirtual.models;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -40,9 +41,216 @@ public class Produto implements Serializable{
 	
 	private Double profundidade;
 	
-	private BigDecimal valorVenda;
+	private BigDecimal valorVenda = BigDecimal.ZERO;
 	
-	private Integer qtdEstoque;
+	private Integer qtdEstoque = 0;
+	
+	private Integer qtdAlertaEstoque = 0;
+	
+	private String linkYoutube;
+	
+	private Boolean alertaQtdEstoque = Boolean.FALSE;
+	
+	private Integer qtdClique = 0;
+	
+	
+	/* Constructor */
+	
+	public Produto() {
+		
+	}
+
+
+	public Produto(Long id, String tipoUnidade, String nome, String descricao, Double peso, Double largura,
+			Double altura, Double profundidade, BigDecimal valorVenda, Integer qtdEstoque, Integer qtdAlertaEstoque,
+			String linkYoutube, Boolean alertaQtdEstoque, Integer qtdClique) {
+		this.id = id;
+		this.tipoUnidade = tipoUnidade;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.peso = peso;
+		this.largura = largura;
+		this.altura = altura;
+		this.profundidade = profundidade;
+		this.valorVenda = valorVenda;
+		this.qtdEstoque = qtdEstoque;
+		this.qtdAlertaEstoque = qtdAlertaEstoque;
+		this.linkYoutube = linkYoutube;
+		this.alertaQtdEstoque = alertaQtdEstoque;
+		this.qtdClique = qtdClique;
+	}
+
+	/* Accessor Methods */
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getTipoUnidade() {
+		return tipoUnidade;
+	}
+
+
+	public void setTipoUnidade(String tipoUnidade) {
+		this.tipoUnidade = tipoUnidade;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public Double getPeso() {
+		return peso;
+	}
+
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+
+	public Double getLargura() {
+		return largura;
+	}
+
+
+	public void setLargura(Double largura) {
+		this.largura = largura;
+	}
+
+
+	public Double getAltura() {
+		return altura;
+	}
+
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+
+	public Double getProfundidade() {
+		return profundidade;
+	}
+
+
+	public void setProfundidade(Double profundidade) {
+		this.profundidade = profundidade;
+	}
+
+
+	public BigDecimal getValorVenda() {
+		return valorVenda;
+	}
+
+
+	public void setValorVenda(BigDecimal valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+
+	public Integer getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+
+	public void setQtdEstoque(Integer qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+
+
+	public Integer getQtdAlertaEstoque() {
+		return qtdAlertaEstoque;
+	}
+
+
+	public void setQtdAlertaEstoque(Integer qtdAlertaEstoque) {
+		this.qtdAlertaEstoque = qtdAlertaEstoque;
+	}
+
+
+	public String getLinkYoutube() {
+		return linkYoutube;
+	}
+
+
+	public void setLinkYoutube(String linkYoutube) {
+		this.linkYoutube = linkYoutube;
+	}
+
+
+	public Boolean getAlertaQtdEstoque() {
+		return alertaQtdEstoque;
+	}
+
+
+	public void setAlertaQtdEstoque(Boolean alertaQtdEstoque) {
+		this.alertaQtdEstoque = alertaQtdEstoque;
+	}
+
+
+	public Integer getQtdClique() {
+		return qtdClique;
+	}
+
+
+	public void setQtdClique(Integer qtdClique) {
+		this.qtdClique = qtdClique;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", tipoUnidade=" + tipoUnidade + ", nome=" + nome + ", descricao=" + descricao
+				+ ", peso=" + peso + ", largura=" + largura + ", altura=" + altura + ", profundidade=" + profundidade
+				+ ", valorVenda=" + valorVenda + ", qtdEstoque=" + qtdEstoque + ", qtdAlertaEstoque=" + qtdAlertaEstoque
+				+ ", linkYoutube=" + linkYoutube + ", alertaQtdEstoque=" + alertaQtdEstoque + ", qtdClique=" + qtdClique
+				+ "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 	
 	
 	
