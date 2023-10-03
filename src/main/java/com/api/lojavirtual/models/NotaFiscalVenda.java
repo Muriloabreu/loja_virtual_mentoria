b992e60ab7219fe7716e491703445a23a700fde2
muriloabreu <murilo.lilo@hotmail.com>
2023-10-02 22:09:55 -0300

    Criação da classe NotaFiscalVenda

---
+++ src/main/java/com/api/lojavirtual/models/NotaFiscalVenda.java
@@ -0,0 +1,148 @@
+package com.api.lojavirtual.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "nota_fiscal_venda")
+@SequenceGenerator(name = "seq_nota_fiscal_venda", sequenceName = "seq_nota_fiscal_venda", allocationSize = 1, initialValue = 1	)
+public class NotaFiscalVenda implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_venda")
+	private Long id;
+	
+	private String numero;
+	
+	private String serie;
+	
+	private String tipo;
+	
+	@Column(columnDefinition = "text")
+	private String xml;
+	
+	@Column(columnDefinition = "text")
+	private String pdf;
+	
+	@OneToOne()
+	@JoinColumn(name = "venda_compra_loja_virt_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_compra_loja_virt_fk") )
+	private VendaCompraLojaVirtual vendaCompraLojaVirtual;
+	
+	
+	/* Constructor */
+	
+	public NotaFiscalVenda() {
+	}
+	
+	public NotaFiscalVenda(Long id, String numero, String serie, String tipo, String xml, String pdf,
+			VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+		this.id = id;
+		this.numero = numero;
+		this.serie = serie;
+		this.tipo = tipo;
+		this.xml = xml;
+		this.pdf = pdf;
+		this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
+	}
+
+
+
+	/* Accessor Methods */
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getSerie() {
+		return serie;
+	}
+
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getXml() {
+		return xml;
+	}
+
+	public void setXml(String xml) {
+		this.xml = xml;
+	}
+
+	public String getPdf() {
+		return pdf;
+	}
+
+	public void setPdf(String pdf) {
+		this.pdf = pdf;
+	}
+	public VendaCompraLojaVirtual getVendaCompraLojaVirtual() {
+		return vendaCompraLojaVirtual;
+	}
+	public void setVendaCompraLojaVirtual(VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+		this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
+	}
+
+	@Override
+	public String toString() {
+		return "NotaFiscalVenda [id=" + id + ", numero=" + numero + ", serie=" + serie + ", tipo=" + tipo + ", xml="
+				+ xml + ", pdf=" + pdf + ", vendaCompraLojaVirtual=" + vendaCompraLojaVirtual + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NotaFiscalVenda other = (NotaFiscalVenda) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
