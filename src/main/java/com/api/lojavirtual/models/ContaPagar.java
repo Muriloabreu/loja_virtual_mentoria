d172d931fd5549f4c9f542546c7bb9b8ea06a0cd
muriloabreu <murilo.lilo@hotmail.com>
2023-10-05 22:01:25 -0300

    Implementação de permissóes para os campos no Banco nas variaveis

--- src/main/java/com/api/lojavirtual/models/ContaPagar.java
+++ src/main/java/com/api/lojavirtual/models/ContaPagar.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -35,17 +36,21 @@ public class ContaPagar implements Serializable{
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
 	private Long id;
 	
+	@Column(nullable = false)
 	private String descricao ;
 	
+	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private StatusContaPagar status;
 	
+	@Column(nullable = false)
 	@Temporal(TemporalType.DATE)
 	private Date dtVencimento;
 	
 	@Temporal(TemporalType.DATE)
 	private Date dtPagamento;
 	
+	@Column(nullable = false)
 	private BigDecimal valorTotal;
 	
 	private BigDecimal valorDesconto;
@@ -53,12 +58,12 @@ public class ContaPagar implements Serializable{
 	
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, 
-	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "paessoa_fk") )
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk") )
 	private Pessoa pessoa;
 	
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_forn_id", nullable = false, 
-	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "paessoa_forn_fk") )
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk") )
 	private Pessoa pessoaFornecedor;
 	
 	
@@ -71,7 +76,6 @@ public class ContaPagar implements Serializable{
 
 	public ContaPagar(Long id, String descricao, StatusContaPagar status, Date dtVencimento, Date dtPagamento,
 			BigDecimal valorTotal, BigDecimal valorDesconto, Pessoa pessoa, Pessoa pessoaFornecedor) {
-		super();
 		this.id = id;
 		this.descricao = descricao;
 		this.status = status;
@@ -124,7 +128,6 @@ public class ContaPagar implements Serializable{
 		return dtVencimento;
 	}
 
-
 	public void setDtVencimento(Date dtVencimento) {
 		this.dtVencimento = dtVencimento;
 	}
@@ -158,7 +161,6 @@ public class ContaPagar implements Serializable{
 	public void setValorDesconto(BigDecimal valorDesconto) {
 		this.valorDesconto = valorDesconto;
 	}
-	
 
 	public Pessoa getPessoaFornecedor() {
 		return pessoaFornecedor;
