6a5f6c7cd7138fc1ddc7a2d1ffbfb0b0e16a6046
muriloabreu <murilo.lilo@hotmail.com>
2023-10-05 21:34:02 -0300

    Criação da classe AvaliacaoProduto

---
+++ src/main/java/com/api/lojavirtual/models/AvaliacaoProduto.java
@@ -0,0 +1,156 @@
+package com.api.lojavirtual.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "avaliacao_produto")
+@SequenceGenerator(name = "seq_avaliacao_produto", sequenceName = "seq_avaliacao_produto", allocationSize = 1, initialValue = 1	)
+public class AvaliacaoProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_avaliacao_produto")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private Integer nota;
+	
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "paessoa_fk") )
+	private Pessoa pessoa;
+	
+	@ManyToOne()
+	@JoinColumn(name = "produto_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk") )
+	private Produto produto;
+	
+	
+	
+	/* Constructor */
+	
+	public AvaliacaoProduto() {
+	}
+
+
+
+	public AvaliacaoProduto(Long id, String descricao, Integer nota, Pessoa pessoa, Produto produto) {
+		this.id = id;
+		this.descricao = descricao;
+		this.nota = nota;
+		this.pessoa = pessoa;
+		this.produto = produto;
+	}
+
+
+	
+	/* Accessor Methods */
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+	public Integer getNota() {
+		return nota;
+	}
+
+
+
+	public void setNota(Integer nota) {
+		this.nota = nota;
+	}
+
+
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "AvaliacaoProduto [id=" + id + ", descricao=" + descricao + ", nota=" + nota + ", pessoa=" + pessoa
+				+ ", produto=" + produto + "]";
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AvaliacaoProduto other = (AvaliacaoProduto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+
+}
