d4fb881f87c46a56e35798744301e4ce7e9f0189
muriloabreu <murilo.lilo@hotmail.com>
2023-10-06 14:44:41 -0300

    Implementação de permissoes nas variaveis, para os campos no Banco de Dados

--- src/main/java/com/api/lojavirtual/models/Produto.java
+++ src/main/java/com/api/lojavirtual/models/Produto.java
@@ -22,27 +22,27 @@ public class Produto implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
 	private Long id;
-	
+	@Column(nullable = false)
 	private String tipoUnidade;
-	
+	@Column(nullable = false)
 	private String nome;
 	
-	@Column(columnDefinition = "text", length = 2000)
+	@Column(columnDefinition = "text", length = 2000, nullable = false)
 	private String descricao;
 	
 	/* Nota Item nota Produto ASSOCIAR*/
 	
-	
+	@Column(nullable = false)
 	private Double peso;
-	
+	@Column(nullable = false)
 	private Double largura;
-	
+	@Column(nullable = false)
 	private Double altura;
-	
+	@Column(nullable = false)
 	private Double profundidade;
-	
+	@Column(nullable = false)
 	private BigDecimal valorVenda = BigDecimal.ZERO;
-	
+	@Column(nullable = false)
 	private Integer qtdEstoque = 0;
 	
 	private Integer qtdAlertaEstoque = 0;
@@ -50,7 +50,7 @@ public class Produto implements Serializable{
 	private String linkYoutube;
 	
 	private Boolean alertaQtdEstoque = Boolean.FALSE;
-	
+	@Column(nullable = false)
 	private Boolean ativo = Boolean.TRUE;
 	
 	private Integer qtdClique = 0;
