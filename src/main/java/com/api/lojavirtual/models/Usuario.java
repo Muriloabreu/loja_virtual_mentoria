f3a0c608d97d2e7eacbccaf88e5da5c8eab139f7
muriloabreu <murilo.lilo@hotmail.com>
2023-10-06 14:45:03 -0300

    Implementação de permissoes nas variaveis, para os campos no Banco de Dados

--- src/main/java/com/api/lojavirtual/models/Usuario.java
+++ src/main/java/com/api/lojavirtual/models/Usuario.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -37,11 +38,11 @@ public class Usuario implements UserDetails{
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_endereco")
 	private Long id;
-	
+	@Column(nullable = false)
 	private String login;
-	
+	@Column(nullable = false)
 	private String senha;
-	
+	@Column(nullable = false)
 	@Temporal(TemporalType.DATE)
 	private Date dataAtualSenha;
 	
