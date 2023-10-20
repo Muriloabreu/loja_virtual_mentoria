4e96908a177af441c61f4d5a612e83432dbceda5
muriloabreu <murilo.lilo@hotmail.com>
2023-10-19 21:38:18 -0300

    Criação do End Point POST para add Acesso na Classe AcessoRepository

--- src/main/java/com/api/lojavirtual/controllers/AcessoController.java
+++ src/main/java/com/api/lojavirtual/controllers/AcessoController.java
@@ -1,19 +1,31 @@
 package com.api.lojavirtual.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.api.lojavirtual.models.Acesso;
 import com.api.lojavirtual.services.AcessoService;
 
 @Controller
+@RestController
 public class AcessoController {
 	
 	@Autowired
 	AcessoService acessoService;
 	
-	public Acesso saveAcesso(Acesso acesso) {
-		return acessoService.save(acesso);
+	@ResponseBody
+	@PostMapping(value = "/salvarAcesso")
+	public ResponseEntity<Acesso>  saverAcesso(@RequestBody Acesso acesso) {
+		
+		Acesso acessoSalvo = acessoService.save(acesso);
+		
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
 	}
 
 }
