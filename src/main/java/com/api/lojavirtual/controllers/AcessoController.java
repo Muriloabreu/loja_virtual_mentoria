cfa1d02f771fa5f5d57dde0d038a929f1676fe47
muriloabreu <murilo.lilo@hotmail.com>
2023-10-26 22:46:12 -0300

    Criação de novos End Points, DELETE, GET

--- src/main/java/com/api/lojavirtual/controllers/AcessoController.java
+++ src/main/java/com/api/lojavirtual/controllers/AcessoController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,10 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.api.lojavirtual.models.Acesso;
 import com.api.lojavirtual.repositories.AcessoRepository;
 import com.api.lojavirtual.services.AcessoService;
-
-
-
-
 
 @Controller
 @RestController
@@ -44,4 +43,23 @@ public class AcessoController {
 		
 		return new ResponseEntity("Acesso removido!" ,HttpStatus.OK);
 	}
+	
+	@ResponseBody
+	@DeleteMapping(value = "/deleteAcessoPorId/{id}")
+	public ResponseEntity<?>  deleteAcessoPorId(@PathVariable("id") Long id) {
+		
+		 acessoRepository.deleteById(id);
+		
+		return new ResponseEntity("Acesso removido!" ,HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@GetMapping(value = "/obterAcesso/{id}")
+	public ResponseEntity<Acesso>  obterAcesso(@PathVariable("id")  Long id) {
+		
+		Acesso acesso = acessoRepository.findById(id).get();
+		
+		return new ResponseEntity<Acesso>(acesso ,HttpStatus.OK);
+	}
+	
 }
