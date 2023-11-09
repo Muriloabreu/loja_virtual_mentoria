ccc9d4392da1911ad7aada23fcc335499d30e81c
muriloabreu <murilo.lilo@hotmail.com>
2023-11-08 23:06:45 -0300

    Implementação de metodos na class WebConfigSecurity

--- src/main/java/com/api/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/com/api/lojavirtual/security/WebConfigSecurity.java
@@ -2,13 +2,18 @@ package com.api.lojavirtual.security;
 
 import javax.servlet.http.HttpSessionListener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.api.lojavirtual.services.ImplementacaoUserDetailsService;
 
 
 
@@ -18,8 +23,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
 	
-
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 	
+	
+	/*CONSULTA USER NO BANCO COM O SPRING SECURITY*/
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+
+	/* IGNORA ALGUMAS URLs - LIVRE DE AUTENTICAÇÃO */
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		
