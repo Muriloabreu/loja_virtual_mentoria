6904b947c2cc0eba6b88855fb89f771c82349f8e
muriloabreu <murilo.lilo@hotmail.com>
2023-11-08 23:08:07 -0300

    Criação de class service ImplementacaoUserDetailsService.java

---
+++ src/main/java/com/api/lojavirtual/services/ImplementacaoUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.api.lojavirtual.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.api.lojavirtual.repositories.UsuarioRepository;
+
+@Service
+public class ImplementacaoUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		return null;
+	}
+
+}
