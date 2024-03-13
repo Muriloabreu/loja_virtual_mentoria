3ddc8e07c8f50c8fc977c9fcad00306b5353bf4a
Muriloabreu <murilo.lilo@hotmail.com>
2024-03-12 21:44:09 -0300

    IMplementação da classe ImplementacaoUserDetailsService, implementando o retorno

--- src/main/java/com/api/lojavirtual/services/ImplementacaoUserDetailsService.java
+++ src/main/java/com/api/lojavirtual/services/ImplementacaoUserDetailsService.java
@@ -1,11 +1,13 @@
 package com.api.lojavirtual.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.api.lojavirtual.models.Usuario;
 import com.api.lojavirtual.repositories.UsuarioRepository;
 
 @Service
@@ -13,11 +15,18 @@ public class ImplementacaoUserDetailsService implements UserDetailsService {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		return null;
+
+		Usuario usuario = usuarioRepository.findUserByLogin(username);
+
+		if (usuario == null) {
+
+			throw new UsernameNotFoundException("Usuário não encontrado");
+		}
+
+		return new User(usuario.getLogin(), usuario.getPassword(), usuario.getAuthorities());
 	}
 
 }
