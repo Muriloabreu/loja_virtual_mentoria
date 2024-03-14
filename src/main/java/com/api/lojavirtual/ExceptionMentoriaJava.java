b6e8e5d193cb26cc068d91ae91afdb26844b47d0
Muriloabreu <murilo.lilo@hotmail.com>
2024-03-14 20:57:50 -0300

    Criação de classe /ExceptionMentoriaJava.java para tratament de excecao

---
+++ src/main/java/com/api/lojavirtual/ExceptionMentoriaJava.java
@@ -0,0 +1,10 @@
+package com.api.lojavirtual;
+
+public class ExceptionMentoriaJava extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionMentoriaJava(String msgErro) {
+		super(msgErro);
+	}
+}
