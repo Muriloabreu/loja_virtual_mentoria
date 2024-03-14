56491e44d5a6b9b7cfc61eb316f6c5f4602aa2ad
Muriloabreu <murilo.lilo@hotmail.com>
2024-03-14 20:54:40 -0300

    implementação na classe JwtApiAutenticacaoFilter para tratar erro de SQL, foi add um try catch no método doFilter()

--- src/main/java/com/api/lojavirtual/security/JwtApiAutenticacaoFilter.java
+++ src/main/java/com/api/lojavirtual/security/JwtApiAutenticacaoFilter.java
@@ -14,21 +14,28 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 public class JwtApiAutenticacaoFilter extends GenericFilterBean {
-	
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
-		/*Estabelece a autenticao do user*/
-		
-		Authentication authentication = new JWTTokenAutenticacaoService().
-				getAuthetication((HttpServletRequest) request, (HttpServletResponse) response);
-		
-		/*Coloca o processo de autenticacao para o spring secutiry*/
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		
-		chain.doFilter(request, response);
-		
+		try {
+			/* Estabelece a autenticao do user */
+
+			Authentication authentication = new JWTTokenAutenticacaoService()
+					.getAuthetication((HttpServletRequest) request, (HttpServletResponse) response);
+
+			/* Coloca o processo de autenticacao para o spring secutiry */
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+
+			chain.doFilter(request, response);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			response.getWriter().write("Ocorreu um erro no Sistema! Contate o administrador: " + e.getMessage());
+		}
+
 	}
 
 }
