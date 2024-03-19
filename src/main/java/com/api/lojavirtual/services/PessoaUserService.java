241e0a512ca96c5e265271ddefeca2342d89130c
Muriloabreu <murilo.lilo@hotmail.com>
2024-03-18 22:54:55 -0300

    criação de classe Service com nome /PessoaUserService

---
+++ src/main/java/com/api/lojavirtual/services/PessoaUserService.java
@@ -0,0 +1,20 @@
+package com.api.lojavirtual.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.lojavirtual.repositories.UsuarioRepository;
+
+@Service
+public class PessoaUserService {
+	
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	
+	
+	
+	
+	
+}
