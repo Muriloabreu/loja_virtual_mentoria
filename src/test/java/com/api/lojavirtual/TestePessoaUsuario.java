a0e63a3051775f4f21d8750d773253f143fe38b3
Muriloabreu <murilo.lilo@hotmail.com>
2024-03-26 23:16:29 -0300

    Criação da classe de Teste PessoaJuridica

--- src/test/java/com/api/lojavirtual/TestePessoaUsuario.java
+++ src/test/java/com/api/lojavirtual/TestePessoaUsuario.java
@@ -1,9 +1,12 @@
 package com.api.lojavirtual;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.api.lojavirtual.models.PessoaFisica;
+import com.api.lojavirtual.models.PessoaJuridica;
+import com.api.lojavirtual.repositories.PessoaRepository;
 import com.api.lojavirtual.services.PessoaUserService;
 
 import junit.framework.TestCase;
@@ -14,16 +17,35 @@ public class TestePessoaUsuario extends TestCase {
 	@Autowired
 	private PessoaUserService pessoaUserService;
 	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	
+	@Test
 	public void testCadPessoa() {
 		
-		PessoaFisica pessoaFisica = new PessoaFisica();
+		PessoaJuridica pessoaJuridica = new PessoaJuridica();
+		
+		pessoaJuridica.setCnpf("111111111111111");
+		pessoaJuridica.setNome("Murilo Abreu");
+		pessoaJuridica.setEmail("abre.murilo@gmail.com");
+		pessoaJuridica.setTelefone("11111111100");
+		pessoaJuridica.setInscEstadual("111111111111111111");
+		pessoaJuridica.setInscMunicipal("11111111111");
+		pessoaJuridica.setNomeFantasia("Empresa AAAAAAAA");
+		pessoaJuridica.setRazaoSocial("Razão aaaaaaaaaaaaaaaaaaaa");
+		pessoaJuridica.setTipoPessoa("Gerente");
+		
+		pessoaRepository.save(pessoaJuridica);
+		
+		
+		PessoaFisica pessoaFisica = new PessoaFisica();		
 		
 		pessoaFisica.setCpf("11111111111");
 		pessoaFisica.setNome("Murilo Abreu");
 		pessoaFisica.setEmail("abre.murilo@gmail.com");
 		pessoaFisica.setTelefone("11111111100");
+		
 		
 		
 		
