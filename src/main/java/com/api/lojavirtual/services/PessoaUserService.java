6cb24e726c3d65f5b6ba30a7c49defe8e8995f26
Muriloabreu <murilo.lilo@hotmail.com>
2024-04-04 23:12:59 -0300

    Implementação de método salvarPessoaJuridica()

--- src/main/java/com/api/lojavirtual/services/PessoaUserService.java
+++ src/main/java/com/api/lojavirtual/services/PessoaUserService.java
@@ -1,8 +1,15 @@
 package com.api.lojavirtual.services;
 
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.api.lojavirtual.models.PessoaJuridica;
+import com.api.lojavirtual.models.Usuario;
+import com.api.lojavirtual.repositories.PessoaRepository;
 import com.api.lojavirtual.repositories.UsuarioRepository;
 
 @Service
@@ -11,9 +18,45 @@ public class PessoaUserService {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private PessoaRepository pesssoaRepository;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public PessoaJuridica salvarPessoaJuridica(PessoaJuridica juridica) {
 
-	
-	
+		juridica = pesssoaRepository.save(juridica);
+
+		Usuario usuarioPj = usuarioRepository.findUserByPessoa(juridica.getId(), juridica.getEmail());
+
+		if (usuarioPj == null) {
+			String constraint = usuarioRepository.consultaConstraintAcesso();
+			if (constraint != null) {
+				jdbcTemplate.execute("begin; alter table usuarios_acesso drop constraint " + constraint + "; commit;");
+			}
+
+		}
+		
+		usuarioPj = new Usuario();
+		usuarioPj.setDataAtualSenha(Calendar.getInstance().getTime());
+		usuarioPj.setEmpresa(juridica);
+		usuarioPj.setPessoa(juridica);
+		usuarioPj.setLogin(juridica.getEmail());
+		
+		String senha = "" + Calendar.getInstance().getTimeInMillis();
+		String senhaCript = new BCryptPasswordEncoder().encode(senha);
+		
+		usuarioPj.setSenha(senhaCript);
+		
+		usuarioPj = usuarioRepository.save(usuarioPj);
+		
+		usuarioRepository.insereAcessoUserPj(usuarioPj.getId());
+		
+		/*Fazer o envio de e-mail do login e da senha*/
+
+	}	
 	
 	
 	
