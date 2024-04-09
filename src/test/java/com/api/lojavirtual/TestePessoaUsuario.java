40027ebae69d5a4fd044fbca7e0f0e90914c0dda
Muriloabreu <murilo.lilo@hotmail.com>
2024-04-08 23:44:02 -0300

    Implementação de Teste SalvarPj Pessoa Juridica

--- src/test/java/com/api/lojavirtual/TestePessoaUsuario.java
+++ src/test/java/com/api/lojavirtual/TestePessoaUsuario.java
@@ -1,9 +1,12 @@
 package com.api.lojavirtual;
 
+import java.util.Calendar;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.api.lojavirtual.controllers.PessoaController;
 import com.api.lojavirtual.models.PessoaFisica;
 import com.api.lojavirtual.models.PessoaJuridica;
 import com.api.lojavirtual.repositories.PessoaRepository;
@@ -20,15 +23,18 @@ public class TestePessoaUsuario extends TestCase {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
+	@Autowired
+	private PessoaController pessoaController;
+	
 	
 	@Test
-	public void testCadPessoa() {
+	public void testCadPessoa() throws ExceptionMentoriaJava {
 		
 		PessoaJuridica pessoaJuridica = new PessoaJuridica();
 		
-		pessoaJuridica.setCnpf("111111111111111");
+		pessoaJuridica.setCnpj("" + Calendar.getInstance().getTimeInMillis());
 		pessoaJuridica.setNome("Murilo Abreu");
-		pessoaJuridica.setEmail("abre.murilo@gmail.com");
+		pessoaJuridica.setEmail("abreu.murilo@gmail.com");
 		pessoaJuridica.setTelefone("11111111100");
 		pessoaJuridica.setInscEstadual("111111111111111111");
 		pessoaJuridica.setInscMunicipal("11111111111");
@@ -36,15 +42,15 @@ public class TestePessoaUsuario extends TestCase {
 		pessoaJuridica.setRazaoSocial("Razão aaaaaaaaaaaaaaaaaaaa");
 		pessoaJuridica.setTipoPessoa("Gerente");
 		
-		pessoaRepository.save(pessoaJuridica);
+		pessoaController.salvarPj(pessoaJuridica);
 		
 		
-		PessoaFisica pessoaFisica = new PessoaFisica();		
-		
-		pessoaFisica.setCpf("11111111111");
-		pessoaFisica.setNome("Murilo Abreu");
-		pessoaFisica.setEmail("abre.murilo@gmail.com");
-		pessoaFisica.setTelefone("11111111100");
+//		PessoaFisica pessoaFisica = new PessoaFisica();		
+//		
+//		pessoaFisica.setCpf("11111111111");
+//		pessoaFisica.setNome("Murilo Abreu");
+//		pessoaFisica.setEmail("abre.murilo@gmail.com");
+//		pessoaFisica.setTelefone("11111111100");
 		
 		
 		
