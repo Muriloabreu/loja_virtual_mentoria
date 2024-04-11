1fc2b2119e5f4af7a20fad457c82c832c3d07951
Muriloabreu <murilo.lilo@hotmail.com>
2024-04-10 22:20:28 -0300

    Implementação na classe de TestePessoaUsuario, teste de cadastro de PessoaJuridica

--- src/test/java/com/api/lojavirtual/TestePessoaUsuario.java
+++ src/test/java/com/api/lojavirtual/TestePessoaUsuario.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.api.lojavirtual.controllers.PessoaController;
+import com.api.lojavirtual.enums.TipoEndereco;
+import com.api.lojavirtual.models.Endereco;
 import com.api.lojavirtual.models.PessoaFisica;
 import com.api.lojavirtual.models.PessoaJuridica;
 import com.api.lojavirtual.repositories.PessoaRepository;
@@ -34,7 +36,7 @@ public class TestePessoaUsuario extends TestCase {
 		
 		pessoaJuridica.setCnpj("" + Calendar.getInstance().getTimeInMillis());
 		pessoaJuridica.setNome("Murilo Abreu");
-		pessoaJuridica.setEmail("abreu.murilo@gmail.com");
+		pessoaJuridica.setEmail("teste01.emp01@gmail.com");
 		pessoaJuridica.setTelefone("11111111100");
 		pessoaJuridica.setInscEstadual("111111111111111111");
 		pessoaJuridica.setInscMunicipal("11111111111");
@@ -42,16 +44,51 @@ public class TestePessoaUsuario extends TestCase {
 		pessoaJuridica.setRazaoSocial("Razão aaaaaaaaaaaaaaaaaaaa");
 		pessoaJuridica.setTipoPessoa("Gerente");
 		
-		pessoaController.salvarPj(pessoaJuridica);
+		
+		Endereco endereco1 = new Endereco();
+		endereco1.setRuaLogra("AV. Visconde A");
+		endereco1.setCep("10010-000");
+		endereco1.setBairro("Centro");
+		endereco1.setNumero("597");
+		endereco1.setComplemento("14 Andar, Sala 1402");
+		endereco1.setCidade("Recife");
+		endereco1.setUf("PE");
+		endereco1.setEmpresa(pessoaJuridica);
+		endereco1.setPessoa(pessoaJuridica);
+		endereco1.setTipoEndereco(TipoEndereco.COBRANCA);
+		
+		Endereco endereco2 = new Endereco();
+		endereco2.setRuaLogra("AV. Visconde A");
+		endereco2.setCep("10010-000");
+		endereco2.setBairro("Centro");
+		endereco2.setNumero("597");
+		endereco2.setComplemento("14 Andar, Sala 1402");
+		endereco2.setCidade("Recife");
+		endereco2.setUf("PE");
+		endereco2.setEmpresa(pessoaJuridica);
+		endereco2.setPessoa(pessoaJuridica);
+		endereco2.setTipoEndereco(TipoEndereco.COBRANCA);
+		
+		pessoaJuridica.getEnderecos().add(endereco1);
+		pessoaJuridica.getEnderecos().add(endereco2);
 		
 		
-//		PessoaFisica pessoaFisica = new PessoaFisica();		
-//		
-//		pessoaFisica.setCpf("11111111111");
-//		pessoaFisica.setNome("Murilo Abreu");
-//		pessoaFisica.setEmail("abre.murilo@gmail.com");
-//		pessoaFisica.setTelefone("11111111100");
+		pessoaController.salvarPj(pessoaJuridica).getBody();
 		
+		assertEquals(true, pessoaJuridica.getId() > 0);
+		
+		//Para testar se salvou os dois endereços
+		for (Endereco endereco : pessoaJuridica.getEnderecos()) {
+			assertEquals(true, endereco.getId() > 0);
+		}
+		
+		//Para testar se salvou dois endereços
+		assertEquals(2, pessoaJuridica.getEnderecos().size());
+		
+		
+		
+		
+
 		
 		
 		
